428dd959cff087b39561b5587c0ce83cd28b96c0
Petir <rasyapradanaputra8@gmail.com>
2026-07-23 19:30:15 +0700

    Model

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -11,39 +11,37 @@ use Illuminate\Database\Eloquent\Relations\HasMany;
 class User extends Authenticatable
 {
     /** @use HasFactory<UserFactory> */
-    use HasFactory, Notifiable;
+    use HasApiTokens, Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var list<string>
-     */
+    protected $table = 'users';
+
     protected $fillable = [
         'name',
         'email',
         'password',
+        'role',
+        'no_hp',
+        'alamat',
+        'foto_profile'
     ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var list<string>
-     */
     protected $hidden = [
         'password',
         'remember_token',
     ];
 
-    /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
-     */
     protected function casts(): array
     {
         return [
-            'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    public function peminjaman(): HasMany {
+        return $this->hasMany(Peminjaman::class);
+    }
+
+    public function logAktivitas(): HasMany {
+        return $this->hasMany(LogAktivitas::class);
     }
 }
