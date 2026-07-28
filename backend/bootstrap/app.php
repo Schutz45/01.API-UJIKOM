c4a2bac42e7dc2bce64d6ed7531c8da74cf7a057
Petir <rasyapradanaputra8@gmail.com>
2026-07-28 11:46:23 +0700

    autentikasi

--- backend/bootstrap/app.php
+++ backend/bootstrap/app.php
@@ -3,6 +3,9 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use App\Http\Middleware\IsAdmin;
+use App\Http\Middleware\IsPetugas;
+use App\Http\Middleware\IsPeminjam;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -12,7 +15,11 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        $middleware->alias([
+            'role.admin'        =>  IsAdmin::class,
+            'role.petugas'      =>  IsPetugas::class,
+            'role.peminjam'     =>  IsPeminjam::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
